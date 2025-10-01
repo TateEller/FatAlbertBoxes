5608f55d39823ca65df18c88b3a510fb084a6b00
TateEller <tce22a@acu.edu>
2025-10-01 14:04:28 -0500

    Comment in TestBox
    Commented out code for using terminal to input dimensions.

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -17,4 +17,29 @@ public class TestBox
         Box box = new Box(30,30, "Test", "Arial");
         assert box != null : "Box creation failed";
     }
+
+    /******
+     * User input from terminal
+     * 
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        float width = scanner.nextFloat();  //will need to check if input is correct type
+
+        System.out.print("Enter height: ");
+        float height = scanner.nextFloat(); //will need to check if input is correct type
+
+        System.out.print("Enter two letter engraving: ");
+        String engraving = scanner.next();  //will need to check if input is correct type
+
+        //float width = 50 , height = 50;
+        //String engraving = "Fat A."; 
+        String font = "";
+        Box test = new Box(width, height, engraving, font);
+        scanner.close();
+        test.print();
+    }
+    ******/
 }
